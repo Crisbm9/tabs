48753f043d4b2c3180a64d0d9268c75307c7d125
Crisbm9 <cristinab.tandem@gmail.com>
2023-11-09 08:44:26 +0100

    archivo

---
+++ bucle-foresch.js
@@ -0,0 +1,8 @@
+const personas=['Kyllian','Dani','Nika'];
+personas.forEach(function(persona,index){
+  console.log(index);
+  console.log(persona);
+});
+personas.forEach((persona,index)=>{
+  console.log(persona);
+});
---
+++ script.js
@@ -0,0 +1,29 @@
+const tabLinks = document.querySelectorAll('.tab-link');
+const tabPanes = document.querySelectorAll('.tab-pane');
+console.log(tabLinks);
+console.log(tabPanes);
+
+tabLinks.forEach(
+  function(link,index){
+    link.addEventListener('click',function(){
+      tabPanes.forEach(function(pane,index){
+            pane.style.display='none';
+      });
+        tabPanes[index].style.display='block';
+      })
+    });
+
+// // Agrega un manejador de eventos a cada pestaña
+// tabLinks.forEach((link, index) => {
+//   link.addEventListener('click', (e) => {
+//       e.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+//       // Oculta todos los paneles de contenido
+//       tabPanes.forEach(pane => {
+//           pane.style.display = 'none';
+//       });
+
+//       // Muestra el panel de contenido correspondiente a la pestaña seleccionada
+//       tabPanes[index].style.display = 'block';
+//   });
+// });
